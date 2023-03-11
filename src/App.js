df1273d8f50f5ede5620f113220a93d5479321b3
RbkHeredia <rbkheredia@gmail.com>
2023-03-11 15:51:10 -0300

    card challenge1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import qr from './assets/images/image-qr-code.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <card className="card">
+        <img src={qr}/>
+        <h1>Improve your front-end skills by building projects</h1>
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </card>
     </div>
   );
 }
